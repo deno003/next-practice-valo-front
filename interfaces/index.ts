339a65c4a7f0c05c00ed9372aa7939900115db38
deno003 <52130732+deno003@users.noreply.github.com>
2021-04-12 16:12:13 +0900

    storybook導入1

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -5,122 +5,125 @@
 // import { User } from 'path/to/interfaces';
 
 export type User = {
-  id: number
-  name: string
-}
+	id: number;
+	name: string;
+};
 
 export type AllContents = {
-  version: string,
-  characters: Content[],
-  maps: Content[],
-  chromas: Content[],
-  skins: Content[],
-  skinLevels: Content[],
-  equips: Content[],
-  gameModes: Content[],
-  sprays: Content[],
-  sprayLevels: Content[],
-  charms: Content[],
-  charmLevels: Content[],
-  playerCards: Content[],
-  playerTitles: Content[],
-  acts: Act[]
-}
+	version: string;
+	characters: Content[];
+	maps: Content[];
+	chromas: Content[];
+	skins: Content[];
+	skinLevels: Content[];
+	equips: Content[];
+	gameModes: Content[];
+	sprays: Content[];
+	sprayLevels: Content[];
+	charms: Content[];
+	charmLevels: Content[];
+	playerCards: Content[];
+	playerTitles: Content[];
+	acts: Act[];
+};
 
 export type Content = {
-  name: string,
-  id: string,
-  assetName: string,
-  assetPath: string
-}
+	name: string;
+	id: string;
+	assetName: string;
+	assetPath: string;
+};
 
 export type ContentProps = {
-  content: Content
-}
+	content: Content;
+};
 
 export type ContentsProps = {
-  contents: Content[]
-}
+	contents: Content[];
+};
 
 export type Act = {
-  id: string,
-  parentId: string,
-  type: string,
-  name: string,
-  isActive: string
-}
+	id: string;
+	parentId: string;
+	type: string;
+	name: string;
+	isActive: string;
+};
 
 export type Agent = {
-  uuid: string,
-  displayName: string,
-  description: string,
-  developerName: string,
-  characterTags: string[],
-  displayIcon: string,
-  displayIconSmall: string,
-  bustPortrait: string,
-  fullPortrait: string,
-  assetPath: string,
-  isFullPortraitRightFacing: boolean,
-  isPlayableCharacter: boolean,
-  isAvailableForTest: boolean,
-  role: Role,
-  abilities: Abilities
-}
+	uuid: string;
+	displayName: string;
+	description: string;
+	developerName: string;
+	characterTags: string[];
+	displayIcon: string;
+	displayIconSmall: string;
+	bustPortrait: string;
+	fullPortrait: string;
+	assetPath: string;
+	isFullPortraitRightFacing: boolean;
+	isPlayableCharacter: boolean;
+	isAvailableForTest: boolean;
+	role: Role;
+	abilities: Abilities;
+};
 
 export type Role = {
-  uuid: string,
-  displayName: string,
-  description: string,
-  displayIcon: string,
-  assetPath: string
-}
+	uuid: string;
+	displayName: string;
+	description: string;
+	displayIcon: string;
+	assetPath: string;
+};
 
 export type Abilities = {
-  slot: string,
-  displayName: string,
-  description: string,
-  displayIcon: string
-}
+	slot: string;
+	displayName: string;
+	description: string;
+	displayIcon: string;
+};
 
 export type AgentProps = {
-  agent: Agent
-}
+	agent: Agent;
+};
 
 export type AgentsProps = {
-  agents: Agent[]
-}
+	agents: Agent[];
+};
 
 export type Map = {
-  uuid: string,
-  displayName: string,
-  coordinates: string,
-  displayIcon: string,
-  listViewIcon: string,
-  splash: string,
-  assetPath: string,
-  mapUrl: string,
-  xMultiplier: string,
-  yMultiplier: string,
-  xScalarToAdd: string,
-  yScalarToAdd: string
-}
+	uuid: string;
+	displayName: string;
+	coordinates: string;
+	displayIcon: string;
+	listViewIcon: string;
+	splash: string;
+	assetPath: string;
+	mapUrl: string;
+	xMultiplier: string;
+	yMultiplier: string;
+	xScalarToAdd: string;
+	yScalarToAdd: string;
+};
 
 export type MapProps = {
-  map:Map
-}
+	map: Map;
+};
 
 export type MapsProps = {
-  maps:Map[]
-}
+	maps: Map[];
+};
 
 export type MapUuidProps = {
-  mapUuid:string
-}
+	mapUuid: string;
+};
 
 export type MapText = {
-  uuid:string,
-  mapName:string,
-  text:string
-}
+	uuid: string;
+	mapName: string;
+	text: string;
+};
 
+export type ButtonProps = {
+	uuid: string;
+};
